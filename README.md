6040cc25a70644e12754ec01825d71fad133a60e
itz-me-fisherYT <126948990+itz-me-fisherYT@users.noreply.github.com>
2023-06-22 21:38:36 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# Fisher-AFK-Minecraft-And-Discord-bot
+
+# Info
+
+A Minecraft bot created with the help of the mineflayer and discord.js libraries.
+
+<p align="center">
+  <a href="https://shields.io"><img src="https://img.shields.io/tokei/lines/github/sagxd/SagXDBot?style=for-the-badge&color=brightgreen&cacheSeconds=3600" alt="forthebadge"/>
+  </a>
+  <a href="https://shields.io"><img src="https://img.shields.io/github/commit-activity/m/sagxd/sagxdbot?style=for-the-badge&color=brightgreen&cacheSeconds=3600" alt="forthebadge"/>
+  </a>
+  <a href="https://shields.io"><img src="https://custom-icon-badges.demolab.com/github/stars/sagxd/sagxdbot?logo=star&style=for-the-badge&color=brightgreen&cacheSeconds=3600" alt="forthebadge"/>
+  </a>
+</p>
+
+<p align="center">
+  <a href="http://forthebadge.com/"><img src="asset/i-love-javascript.svg" alt="forthebadge"/></a>
+</p>
+
+## Getting started
+1. Install [node.js](https://nodejs.org/) and npm
+2. Clone this repository
+3. Configurate your bot in config.json
+4. Install dependencies using `npm i`
+5. Run the bot with `node .`
+
+## Config format
+```json
+{
+  "Dtoken": "YOU_DISCORD_BOT_TOKEN",
+  "prefix": "PREFIX_FOR_DISCORD_BOT",
+  "username": "BotName",
+  "password": false,
+  "ip": "Server_IP",
+  "version": "1.19",
+  "scid": "CHANNEL_ID_FOR_CHAT_BRIDGE",
+  "jc": "",
+  "messages": [
+    "message1",
+    "message2"
+  ],
+  "colorcodes": "&k!!!",
+  "blacklist": [
+    "player1",
+    "player2"
+  ],
+  "delay": 20
+}
+```
+## Commands
+
+- ttt - Play Tic-Tac-Toe.
+- spam - Spam Ping The Mention User.
+- dupe - Perform Frame Dupe.
+- say -  Use Discord to Chat in Minecraft Server.
+- movement - To See all Movement Commands.
+- online - Get List of Online Player.
+- tps - Get current TPS(TickPerSecond) of Server.
+- help - Get List of All Available Commands.
+- location - Get current location of bot.
+- status - Get current status of bot like hunger and health.
+- purge - Delete Messages in Bulk.
+
+## FAQ
+
+#### Why Discord.js V12?
+
+Because I am dumb, I accidentially wrote this using Discord.js v12.
+
+#### Does the bot have Whisper Spam?
+Yep
+
+## Thanks To
+[PrismarineJS](https://github.com/PrismarineJS) for [Mineflayer](https://github.com/PrismarineJS/mineflayer)
+
+[DiscordJS](https://github.com/discordjs/) for [discord.js](https://github.com/discordjs/discord.js/)
+
+[TomPlaneta](https://github.com/TomPlaneta/) for [SpamBot+](https://github.com/TomPlaneta/SpamBotPlus)
+
+#### NOTE - This project is still not finished. I will add more commands
---
+++ index.js
@@ -0,0 +1,400 @@
+const Dtoken = process.env['Dtoken']
+var mineflayer = require('mineflayer');
+var tpsPlugin = require('mineflayer-tps')(mineflayer);
+var antiafk = require('mineflayer-antiafk');
+
+
+var D = require('discord.js');
+var ttt = require('discord-tictactoe');
+var chalk = require('chalk');
+
+const client = new D.Client({ intents: [] });
+const config = require('./config.json');
+let color = "#RANDOM";
+
+client.setMaxListeners(25);
+
+var jc = config.jc;
+let prefix = config.prefix;
+let ip = config.ip;
+let username = config.username;
+let ver = config.version;
+var bot;
+function createBot() {
+  let pass = config.password;
+  if (!pass) {
+    bot = mineflayer.createBot({
+      host: ip,
+      port: 46296,
+      hideErrors: false,
+      username: username,
+      version: ver
+    });
+  } else {
+    bot = mineflayer.createBot({
+      host: ip,
+      port: 46296,
+      username: username,
+      hideErrors: false,
+      password: pass,
+      version: ver
+    });
+  }
+
+  bot.loadPlugin(tpsPlugin);
+  bot.loadPlugin(antiafk);
+
+
+  bot.on('playerJoined', (player) => {
+    if (player.username !== bot.username) {
+      bot.chat(`Hello! ${player.username}`)
+    }
+  });
+
+  bot.on('playerLeft', (player) => {
+  if (player.username === bot.username) return
+  bot.chat(`Bye come back later! ${player.username}.`)
+});
+  
+
+  // =========================
+  // SET ACTIVITY BOT
+  // =========================
+  client.on('ready', activity => {
+    client.user.setStatus(`online`)
+    client.user.setActivity(
+      `${ip} Servers Chat, | For Help Do  ${prefix}help `, {
+      type: "WATCHING"
+    }
+    )
+  });
+
+  client.on('ready', async () => {
+    console.log(chalk.blue('=-()-=-=()=--=()=--=()=--=()=--=()=-()-=-=()=--=()=--=()=--=()=--=()'))
+    console.log(chalk.magenta(`Discord Bot on. Loggined as ${client.user.tag}`))
+    console.log(chalk.red('=-()-=-=()=--=()=--=()=--=()=--=()=-()-=-=()=--=()=--=()=--=()=--=()\n'))
+  })
+  bot.once('login', async () => {
+    console.log(chalk.blue('=-()-=-=()=--=()=--=()=--=()=--=()=-()-=-=()=--=()=--=()=--=()=--=()'))
+    console.log(chalk.magenta(`bot is on At ${ip}`))
+    bot.chat(jc)
+    console.log(chalk.yellow(`Bot just say "${jc}"`))
+    console.log(chalk.red('=-()-=-=()=--=()=--=()=--=()=--=()=-()-=-=()=--=()=--=()=--=()=--=()\n'))
+  })
+
+  let forceStop = false;
+
+  function getRandomMessage() {
+    return config.messages[Math.floor(Math.random() * config.messages.length)];
+  }
+
+  function isInLobby() {
+    if (!bot || !bot.game || bot.game.difficulty != "hard") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bot.once("login", async () => {
+    await bot.waitForTicks(1000);
+    while (!forceStop) {
+      if (!isInLobby()) {
+        for (player in bot.players) {
+          const targetUsername = bot.players[player].username;
+          if (config.blacklist.includes(targetUsername.toLowerCase()) || bot.entity.username == targetUsername) continue;
+          if (forceStop) return;
+          bot.chat(getRandomMessage());
+          await bot.waitForTicks(config.delay);
+        }
+      }
+      await bot.waitForTicks(2);
+    }
+  });
+
+  //tictactoe
+  new ttt({
+    "language": "en",
+    "command": "-ttt",
+    "commandOptionName": "opponent",
+  }).attach(client)
+
+  const bossName = ["itz_me_fisherYT"];
+
+  bot.on("chat", async function(username, message) {
+    if (!bossName.includes(username)) return;
+
+    let msg = message.toString();
+    let args = msg.split(' ');
+
+    if (msg.startsWith("!")) {
+      let text = msg.substring(1); // Remove the first character "!"
+
+      // Remove color codes from the message
+      text = text.replace(/§[0-9a-fklmnor]/gi, '');
+      if (text === "boss") {
+        bot.chat(`my owner is > ${bossName}`);
+        bot.chat("i am his slave!");
+        bot.chat("itz_me_fisherYT i am going to tpa! {if there is /tpa}");
+        bot.chat("/tpa itz_me_fisherYT");
+      }
+      if (text === "discord") {
+        bot.chat("join the discord! https://discord.gg/j9WJ9F8KnM !");
+      }
+      if (text === 'leave') {
+        bot.chat("ill rejoin in 10 seconds");
+        bot.quit();
+      }
+      if (text === 'tips') {
+        bot.chat("remember to see your bed! there is not /sethome, /home");
+        bot.chat("no /tpa no /enderchest");
+      }
+      if (text === 'help') {
+        bot.chat("my commands are !boss, !discord, !tips, and !leave. :D");
+      }
+    }
+  });
+
+
+  client.on('message', msg => {
+    if (!msg.content.startsWith(prefix)) return
+    let args = msg.content.split(" ").slice(1)
+    args = msg.content.slice(prefix.length).split(/ +/);
+    let command = msg.content.split(" ")[0];
+    command = command.slice(prefix.length);
+    command = args.shift().toLowerCase();
+    if (command == "say") {
+      const chat = args.join(" ")
+      bot.chat("" + chat)
+      const success = new D.MessageEmbed()
+        .setDescription(`:white_check_mark: ${msg.author.tag} sent \`${chat}\``)
+        .setTimestamp();
+      msg.channel.send(success);
+    } else if (command == "forward") {
+      bot.setControlState('forward', true)
+      const MoForw = new D.MessageEmbed()
+        .setDescription(`:white_check_mark: Im Moving forward To Stop Do -stop`)
+        .setTimestamp();
+      msg.channel.send(MoForw);
+    } else if (command == "backward") {
+      bot.setControlState('back', true)
+      const MoBackw = new D.MessageEmbed()
+        .setDescription(`:white_check_mark: Im Moving backward To Stop Do -stop`)
+        .setTimestamp();
+      msg.channel.send(MoBackw);
+    } else if (command == "stop") {
+      bot.clearControlStates()
+      const MoStop = new D.MessageEmbed()
+        .setDescription(`:white_check_mark: Stopped!`)
+        .setTimestamp();
+      msg.channel.send(MoStop);
+    } else if (command == "left") {
+      bot.setControlState('left', true)
+      const MoLeft = new D.MessageEmbed()
+        .setDescription(`:white_check_mark: Im Moving left To Stop Do -stop`)
+        .setTimestamp();
+      msg.channel.send(MoLeft);
+    } else if (command == "right") {
+      bot.setControlState('right', true)
+      const MoRight = new D.MessageEmbed()
+        .setDescription(`:white_check_mark: Im Moving Right To Stop Do -stop`)
+        .setTimestamp();
+      msg.channel.send(MoRight);
+    } else if (command == "help") {
+      const help = new D.MessageEmbed()
+        .setTitle('Help')
+        .setDescription('Here are the available commands:')
+        .addFields({
+          name: `:speech_balloon: ${prefix}say`,
+          value: `Get the bot to say what you want.`
+        }, {
+          name: `:arrows_counterclockwise: ${prefix}movement`,
+          value: `Look at the movement command.`
+        }, {
+          name: `:frame_photo: ${prefix}dupe`,
+          value: `Frame dupe. `
+        }, {
+          name: `:signal_strength: ${prefix}tps`,
+          value: `Get TPS of server.`
+        }, {
+          name: `:busts_in_silhouette: ${prefix}online`,
+          value: `Get the current player list.`
+        }, {
+          name: `:information_source: ${prefix}status`,
+          value: `Show information about the bot.`
+        }, {
+          name: `:world_map: ${prefix}location`,
+          value: `Show the bot's current location.`
+        }, {
+          name: `:loudspeaker: ${prefix}spam`,
+          value: `Spam ping the mentioned user. (Usage: ${prefix}spam @username 10)`
+        }, {
+          name: `:wastebasket: ${prefix}purge`,
+          value: `Delete messages in bulk.`
+        }, {
+          name: `:game_die: ${prefix}ttt`,
+          value: `Play TicTacToe.`
+        },);
+      msg.channel.send(help);
+    } else if (command == "movement") {
+      const movement = new D.MessageEmbed()
+        .setTitle(`Movement Command`)
+        .addFields({
+          name: ` :arrow_up: ${prefix}forward`,
+          value: `To Move Forward.`
+        }, {
+          name: ` :arrow_down: ${prefix}backward`,
+          value: `To Move Backward `
+        }, {
+          name: ` :arrow_left:  ${prefix}left`,
+          value: `To Move Left.`
+        }, {
+          name: ` :arrow_right: ${prefix}right`,
+          value: `To Mover Right`
+        }, {
+          name: ` :stop_button: ${prefix}stop`,
+          value: `To Stop all Movement`
+        }, {
+          name: ` :kangaroo: ${prefix}jump`,
+          value: `To Jump`
+        }, {
+          name: ` :stop_button: ${prefix}stopjump`,
+          value: `To Stop`
+        }, {
+          name: ` :feet: ${prefix}sneak`,
+          value: `To Sneak`
+        }, {
+          name: ` :stop_button: ${prefix}stopsneak`,
+          value: `To Stop Sneak`
+        },);
+      msg.channel.send(movement);
+    } else if (command == "purge") {
+      msg.channel.bulkDelete(100);
+    } else if (command == "dupe") {
+      const itemframe = bot.nearestEntity(entity => entity.name.match('item_frame'))
+      if (itemframe) {
+        setInterval(async () => {
+          const shulk = bot.inventory.items().find(item => item.name.includes('shulker_box'))
+          if (shulk) {
+            bot.equip(shulk, 'hand')
+            bot.lookAt(itemframe.position);
+            await bot.useOn(itemframe)
+            await bot.attack(itemframe)
+          }
+        }, 300);
+        const dupe = new D.MessageEmbed()
+          .setDescription(`:white_check_mark: Dupe Started`);
+        msg.channel.send(dupe);
+      } else {
+        return msg.reply("Bot doesn't have Shulker Boxes or Bot is not near a Item Frame.");
+      }
+    } else if (command == "tps") {
+      const tps = new D.MessageEmbed()
+        .setDescription(`Current TPS of the server is **${bot.getTps()}**`);
+      msg.channel.send(tps);
+    } else if (command == "online") {
+      let players = Object.keys(bot.players);
+      let list = players.map((e) => e.username);
+      const playerList = players.join(", ");
+
+      const online = new D.MessageEmbed()
+        .setTitle(`List Of Online Players`)
+        .setDescription(`Here are the players currently online:\n\n${playerList}`);
+
+      if (playerList) {
+        return msg.channel.send(online);
+      } else {
+        return msg.channel.send("There are no players online right now.");
+      }
+    } else if (command == "status") {
+      const status = new D.MessageEmbed()
+        .setTitle(`**${bot.username}** Statistics`)
+        .setDescription(`Health: **${bot.health}**\nFood: **${bot.food}**\nXP: **${bot.experience.level || 0}**`);
+
+      msg.channel.send(status);
+    } else if (command == "location") {
+      const X = bot.entity.position.x.toFixed(1);
+      const Y = bot.entity.position.y.toFixed(1);
+      const Z = bot.entity.position.z.toFixed(1);
+
+      const coord = new D.MessageEmbed()
+        .setTitle(`**${bot.username}** Location`)
+        .setDescription(`X: **${X}**\nY: **${Y}**\nZ: **${Z}**`);
+      msg.channel.send(coord);
+    } else if (command == "spam") {
+      const numMessages = parseInt(args[1]);
+      const user = msg.mentions.users.first();
+
+      if (!user) {
+        return msg.reply('Please mention a user to spam!');
+      }
+
+      if (isNaN(numMessages) || numMessages < 1 || numMessages > 10000) {
+        return msg.reply('Please provide a valid number of messages between 1 and 10000.');
+      }
+
+      for (let i = 0; i < numMessages; i++) {
+        msg.channel.send(`Hey ${user}! You're being spammed!`);
+      }
+    } else if (command == "sneak") {
+      bot.setControlState('sneak', true);
+      const sneak = new D.MessageEmbed()
+        .setDescription(`Bot is Sneaking`)
+      msg.channel.send(sneak);
+    } else if (command == "stopsneak") {
+      bot.setControlState('sneak', false);
+      const stsneak = new D.MessageEmbed()
+        .setDescription(`Bot Stopped Sneaking`)
+      msg.channel.send(stsneak);
+    } else if (command == "jump") {
+      bot.setControlState('jump', true);
+      const jump = new D.MessageEmbed()
+        .setDescription(`Bot Started Jumping`)
+      msg.channel.send(jump);
+    } else if (command == "stopjump") {
+      bot.setControlState('jump', false);
+      const stjump = new D.MessageEmbed()
+        .setDescription(`Bot Stopped Jumping`)
+      msg.channel.send(stjump);
+    } else if (command == "antiafk"){
+        const afk = ['jump']
+        bot.afk.setOptions({ actions: afk});
+        bot.afk.setOptions({ maxActionsInterval: 2000});
+        bot.afk.start();
+        const start = new D.MessageEmbed()
+            .setDescription(`AFK Started`)
+        msg.channel.send(start);
+    } else if (command =="stopafk"){
+        bot.afk.stop();
+        const stop = new D.MessageEmbed()
+            .setDescription(`AFK Stopped`)
+        msg.channel.send(stop);
+    }
+  })
+
+  bot.on("messagestr", message => {
+    let channel = client.channels.cache.get(config.scid)
+    if (!channel) return;
+    const msg = new D.MessageEmbed()
+      .setDescription(`>> ${message}`)
+    channel.send(msg)
+  });
+
+  client.login(Dtoken)
+    .catch(error => {
+      console.log(chalk.red(`Can't Login`));
+    })
+  
+  bot.on('kicked', (reason) => {
+    console.log(chalk.red(`[BotLog] kicked from the server. Reason: \n${reason}`));
+  });
+  
+  bot.on('end', () => {
+    console.log(chalk.blue('Disconnected, attempting to reconnect in 10 seconds...'));
+    setTimeout(() => {
+      createBot(); // Recreate the bot
+    }, 10000); // Reconnect after 10 seconds
+  });
+  //end of code
+}
+createBot();
